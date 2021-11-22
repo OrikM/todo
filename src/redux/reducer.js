20d27b10df175b98c84f7304fdc8bfae92962b3e
Orkhan <omehraliyev@gmail.com>
2021-11-22 15:46:00 +0400

    FİNAL COMMİT

---
+++ src/redux/reducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const addTodoReducer = createSlice({
+  name: "todo",
+
+  initialState,
+
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+      return state;
+    },
+  },
+});
+
+export const { addTodo } = addTodoReducer.actions;
+export const reducer = addTodoReducer.reducer;
